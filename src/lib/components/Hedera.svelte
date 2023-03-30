1f5dd37ea2bb8f78c887f331d28f4fa785b249d5
Sgandhi10 <sohamgandhi@live.com>
2023-03-29 22:17:52 -0400

    Updated GUI

--- src/lib/components/Hedera.svelte
+++ src/lib/components/Hedera.svelte
@@ -6,19 +6,24 @@
     let response = "";
     let file : HTMLInputElement;
     let error = "";
+    let smartContract = ""
 
 </script>
 <div class="m-4 my-auto border-2 border-gray-600 rounded-md p-8">
     <h1>Schedule an update to Hedera</h1>
 
+    <div class="mt-4">
+        <label for="smartContract" class="mr-4">Smart Contract:</label>
+        <input type="text" id="fileHash" name="fileHash" placeholder="Select Smart Contract" class="bg-black" bind:value="{smartContract}">
+    </div>
     <div class="mt-4 ">
-        <label for="file" class="mr-4">Update:</label>
+        <label for="update" class="mr-4">Update:</label>
         <input type="text" id="name" name="name" placeholder="Select Update" class="bg-black" bind:value="{updateVersion}">
     </div>
 
     <div class="mt-4">
-        <label for="file" class="mr-4">Device:</label>
-        <input type="text" id="fileHash" name="fileHash" placeholder="Select Device" class="bg-black" bind:value="{device}">
+        <label for="device" class="mr-4">Device:</label>
+        <input type="text" id="device" name="device" placeholder="Select Device" class="bg-black" bind:value="{device}">
     </div>
 
 </div>
